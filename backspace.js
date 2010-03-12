61fb9cb4acfdb610341b75831e40c41ceb73d160
me <me@noctarius.com>
2010-03-12 09:43:53 +0000

    Import of version 0.1

---
+++ backspace.js
@@ -0,0 +1,30 @@
+﻿document.onkeydown = BackspaceKeyListener;
+document.onkeyup = BackspaceKeyListener;
+document.onkeypress = BackspaceKeyListener;
+
+function BackspaceKeyListener(event) {
+	var isCtrl = event.ctrlKey;
+	var isAlt = event.altKey;
+	var isShift = event.shiftKey;
+	
+	if (!isCtrl && !isAlt && !isShift) {
+		var target = event.target;
+		if (event.which == 8 && target) {		
+			// If on text fields enable usage
+			if (target.type == 'text' ||
+					target.type == 'textarea')
+				return true;
+			
+			else
+				chrome.extension.sendRequest( { message: "isActivated" }, 
+					function(response) {
+						console.log(response.message);
+						if (response.message == true)
+							window.history.back();
+					}
+				);
+		}
+	}
+	
+	return true;
+}
